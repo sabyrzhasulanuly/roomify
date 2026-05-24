53618fbe4956ffc7e03369970ef8595a1387a0be
Sabyrzhan Zhaslanuly <sabyrzhasulanuly@gmail.com>
2026-05-25 02:21:25 +0500

    feat: add projects section to home page with cards and new icons integration

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,6 @@
 import type { Route } from './+types/home'
 import { Navbar } from '../../components/Navbar'
-import { ArrowRight, Layers } from 'lucide-react'
+import { ArrowRight, ArrowUpRight, Clock, Layers } from 'lucide-react'
 import { Button } from '../../components/ui/Button'
 
 export function meta({}: Route.MetaArgs) {
@@ -45,6 +45,42 @@ export default function Home() {
               <p>Supports JPG, PNG, formats up to 10MB</p>
             </div>
             <p>Upload images</p>
+          </div>
+        </div>
+      </section>
+      <section className="projects">
+        <div className="section-inner">
+          <div className="section-head">
+            <div className="copy">
+              <h2>Projects</h2>
+              <p>Your latest work and shared community projects, all in one place.</p>
+            </div>
+          </div>
+          <div className="projects-grid">
+            <div className="project-card group">
+              <div className="preview">
+                <img
+                  src="https://roomify-mlhuk267-dfwu1i.puter.site/projects/1770803585402/rendered.png"
+                  alt="Project"
+                />
+                <div className="badge">
+                  <span>Community</span>
+                </div>
+              </div>
+              <div className="card-body">
+                <div>
+                  <h3>Project Manhattan</h3>
+                  <div className="meta">
+                    <Clock size={12} />
+                    <span>{new Date('01.01.2027').toLocaleString()}</span>
+                    <span>By Sabyrka</span>
+                  </div>
+                </div>
+                <div className="arrow">
+                  <ArrowUpRight size={18} />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
