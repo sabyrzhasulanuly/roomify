27e2ea66cbe442042c7ae577892243d9458ecf5d
Sabyrzhan Zhaslanuly <sabyrzhasulanuly@gmail.com>
2026-05-25 04:09:16 +0500

    feat: enhance Visualizer route to handle dynamic ID with validation

--- app/routes/visualizer.$id.tsx
+++ app/routes/visualizer.$id.tsx
@@ -1,3 +1,11 @@
+import { useParams } from "react-router";
+
 export default function VisualizerId() {
-  return <div>Visualizer Id</div>
+  const { id } = useParams();
+
+  if (!id) {
+    return <div>Invalid ID</div>;
+  }
+
+  return <div>Visualizer Id: {id}</div>;
 }
